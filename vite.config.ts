ce9ff840bbfa57926ef123f57131ae064d52c779
Julius van Voorden <juliusvanvoorden@outlook.com>
2026-07-04 11:17:56 +0200

    upload to itch

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
 import childProcess from "child_process";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 const commitHash = childProcess
   .execSync("git log -1 --pretty=%B | cat")
@@ -10,8 +11,15 @@ const commitHash = childProcess
 console.log("commit hash", commitHash);
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [wasm(), topLevelAwait(), viteSingleFile()],
   define: {
     COMMIT_NAME: JSON.stringify(commitHash),
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        paths: (id) => id,
+      },
+    },
   },
 });
